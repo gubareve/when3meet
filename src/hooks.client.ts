fd43379e8a46b619731d13eb13762e2781699ad3
Nv7 <50369270+Nv7-GitHub@users.noreply.github.com>
2023-02-06 18:36:12 +0000

    Load from cookie

--- src/hooks.client.ts
+++ src/hooks.client.ts
@@ -1,5 +1,7 @@
 import { pb, user } from '$lib/pb';
 
+pb.authStore.loadFromCookie(document.cookie);
+
 pb.authStore.onChange(async () => {
 	user.set(pb.authStore.model);
 	document.cookie = pb.authStore.exportToCookie({ httpOnly: false });
