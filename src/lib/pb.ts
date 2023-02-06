a7405c557a8b3d8c5e0a64ac80a8402666f8ec08
johan <johan@example.com>
2023-02-05 17:15:34 -0800

    Fix DB URL

--- src/lib/pb.ts
+++ src/lib/pb.ts
@@ -1,7 +1,7 @@
 import PocketBase from 'pocketbase';
 import {writable} from "svelte/store";
 
-export const pbUrl = 'http://localhost:8090/'
+export const pbUrl = 'http://127.0.0.1:8090/'
 export const pb = new PocketBase(pbUrl);
 pb.autoCancellation(false);
 export const user = writable(pb.authStore.model);
